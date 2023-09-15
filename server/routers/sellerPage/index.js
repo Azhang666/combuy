d050fccf65f099ce7fb887affe8c70ade98d6eb6
AA9839 <a41.aa9839@gmail.com>
2023-09-15 16:26:32 +0800

    修改首頁

--- server/routers/sellerPage/index.js
+++ server/routers/sellerPage/index.js
@@ -1,22 +1,24 @@
-const express = require("express");
-const path = require("path");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-const db = require("./server/routes/database"); // 引入資料庫連接模組
-const productRoutes = require("./server/routes/productRoutes"); // 引入路由模組
+const db = require('./server/routes/database') // 引入資料庫連接模組
+const productRoutes = require('./server/routes/productRoutes') // 引入路由模組
+// const router = require('../api')
 
-const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, "public")));
+const router = express.Router()
+// const app = express()
+router.use(cors())
+router.use(bodyParser.json())
+router.use(express.static(path.join(__dirname, 'public')))
 
-app.use("/", productRoutes); // 使用路由模組
+router.use('/', productRoutes) // 使用路由模組
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
-});
+router.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
 
 // const PORT = 2407;
 // app.listen(PORT, () => {
@@ -26,3 +28,4 @@ app.use((err, req, res, next) => {
 //   console.log("------------------------------------");
 //   console.log("------------------------------------");
 // });
+module.exports = router
