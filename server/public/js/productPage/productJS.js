9247b1fcdc9d3b34b21e8cf9c5489b61e11a559f
Azhang <jertasdcvb@gmail.com>
2023-09-17 17:39:59 +0800

    新增比較功能

--- server/public/js/productPage/productJS.js
+++ server/public/js/productPage/productJS.js
@@ -214,29 +214,54 @@ $(document).ready(function () {
     });
   });
   //req product comparison
-  $("#prodComparison").on("click", function () {
-    // $.ajax({
-    //   type: "get",
-    //   url: "/search",
-    //   data: {
-    //     search: $(this).val(),
-    //   },
-    //   success: function (response) {
-    //     return;
-    //     var values = {};
-    //     for (index in response) {
-    //       values[index] = response[index].prod;
-    //     }
-    //     let result = ``;
-    //     for (let i = Object.values(values).length - 1; i >= 0; i--) {
-    //       result += `<li><a href="/${values[i]}">${values[i]}</a></li>`;
-    //     }
-    //     result == ""
-    //       ? (result = "<li><a href='/'>搜尋無結果</a></li>")
-    //       : (result = result);
-    //     $("#search-result>ul").html(result);
-    //   },
-    // });
+  $(".prodComparison").on("click", async function () {
+    await $.ajax({
+      type: "get",
+      url: "/api/changeProduct/prodComparison",
+      data: {
+        prod_id: $(this).data("prod_id"),
+        spec_id: $(this).data("spec_id"),
+      },
+      success: function (response) {
+        let arrTemp = [];
+        let objTemp = {
+          pId: response[0].prod_id,
+          sId: response[0].spec_id,
+          pName: response[0].prod_name,
+          sName: response[0].spec_name,
+          brand: response[0].brand,
+          cpu: response[0].cpu,
+          gpu: response[0].gpu,
+          ram: response[0].ram,
+          os: response[0].os,
+          screen: response[0].screen,
+          battery: response[0].battery,
+          size: response[0].size,
+          weight: response[0].weight,
+          warranty: response[0].warranty,
+          imgSrc: response[0].img_src,
+        };
+        if (!localStorage.getItem("product")) {
+          localStorage.setItem("product", JSON.stringify(objTemp));
+          //display btn
+        } else {
+          if (Array.isArray(JSON.parse(localStorage.getItem("product")))) {
+            arrTemp = JSON.parse(localStorage.getItem("product"));
+            if (arrTemp.length == 3) {
+              arrTemp.shift();
+            }
+          } else {
+            arrTemp.push(JSON.parse(localStorage.getItem("product")));
+          }
+          arrTemp.push(objTemp);
+          localStorage.setItem("product", JSON.stringify(arrTemp));
+        }
+        // console.log(JSON.parse(localStorage.getItem("product")));
+      },
+      error: function (error) {
+        alert(error);
+      },
+    });
   });
   //api/changeProductItem and this button prevent bubble events防止泡沫事件
   $("body").on("click", ".prodToPItem", function (e) {
@@ -248,7 +273,7 @@ $(document).ready(function () {
   });
   let user_id = $("#userId").data("userId");
   //plusProduct
-  $("#plusProd").on("click", async function () {
+  $(".plusProd").on("click", async function () {
     if (user_id == "") {
       location.replace("http://localhost:2407/login");
     } else {
@@ -270,7 +295,7 @@ $(document).ready(function () {
     }
   });
   //collectProduct
-  $("#collectProd").on("click", async function () {
+  $(".collectProd").on("click", async function () {
     await $.ajax({
       type: "post",
       url: "/commodity/addcollect",
