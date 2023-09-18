e7d910be6eeb8bc37b148470acfcffd4d42024b4
Lu <jertasdcvb@gmail.com>
2023-09-18 14:13:08 +0800

    整合＆更新產品比較

--- server/routers/productPage/productPage.js
+++ server/routers/productPage/productPage.js
@@ -5,4 +5,8 @@ const router = express.Router();
 //product page
 router.get("/:brand", cpp.ctrlProdPage);
 
+router.get("/watchComparison", (req, res) => {
+  res.render("product/watchComparison.ejs");
+});
+
 module.exports = router;
