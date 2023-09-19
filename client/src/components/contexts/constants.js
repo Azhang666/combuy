bf1e831f34b94a09ea703e50b3d4860a2e12528f
AA9839 <a41.aa9839@gmail.com>
2023-09-19 13:21:39 +0800

    set proxy

--- client/src/components/contexts/constants.js
+++ client/src/components/contexts/constants.js
@@ -1,37 +1,37 @@
 // API Endpoints
 
-const BASE_URL = 'http://localhost:2407';
+// const BASE_URL = 'http://localhost:2407/seller'
+const BASE_URL = '/seller'
 
 export const API_ENDPOINTS = {
-    LOCALHOST: BASE_URL,
-    PRODUCT: `${BASE_URL}/products`,
-    PRODUCT_BY_ID: (id) => `${BASE_URL}/getProductsAllData/${id}`, 
-    CREATE_PRODUCT: `${BASE_URL}/createProduct`,
-    CATEGORY: `${BASE_URL}/category`,        
-    BRAND: `${BASE_URL}/brand`,               
-    DOWN: `${BASE_URL}/down`,
-    DOWN_API: `${BASE_URL}/downproducts`,
-    UPDATE_PRODUCT_PARTIALLY: (id) => `${BASE_URL}/updateProductPartially/${id}`
-
-};
+  LOCALHOST: BASE_URL,
+  PRODUCT: `${BASE_URL}/products`,
+  PRODUCT_BY_ID: id => `${BASE_URL}/getProductsAllData/${id}`,
+  CREATE_PRODUCT: `${BASE_URL}/createProduct`,
+  CATEGORY: `${BASE_URL}/category`,
+  BRAND: `${BASE_URL}/brand`,
+  DOWN: `${BASE_URL}/down`,
+  DOWN_API: `${BASE_URL}/downproducts`,
+  UPDATE_PRODUCT_PARTIALLY: id => `${BASE_URL}/updateProductPartially/${id}`,
+}
 
 // User
-export const USER_ID = 1;
+export const USER_ID = 1
 
 // Checkbox Option
 export const PAYMENT_OPTIONS = [
-    { id: "bank_transfer", label: "銀行或郵局轉帳", value: "0" },
-    { id: "credit_card", label: "信用卡(一次付清)", value: "1" }
-];
+  { id: 'bank_transfer', label: '銀行或郵局轉帳', value: '0' },
+  { id: 'credit_card', label: '信用卡(一次付清)', value: '1' },
+]
 
 export const TRANSPORT_OPTIONS = [
-    { id: "postal_delivery", label: "郵寄寄送", value: "0" },
-    { id: "courier_delivery", label: "宅配/快遞", value: "1" }
-];
+  { id: 'postal_delivery', label: '郵寄寄送', value: '0' },
+  { id: 'courier_delivery', label: '宅配/快遞', value: '1' },
+]
 
 export const FAILSTEXT = '操作失敗，請再試一次。'
 
 export const SAVE = {
-    SAVE: '儲存',
-    SAVEANDPUBLISH: '儲存並上架'
+  SAVE: '儲存',
+  SAVEANDPUBLISH: '儲存並上架',
 }
--- server/routers/sellerPage/routes/productController.js
+++ server/routers/sellerPage/routes/productController.js
@@ -1,18 +1,18 @@
-const dbQuery = require("./dbUtils");
-const upload = require("./multerConfig");
-const db = require("./database");
-const { handleDbError } = require("./errorHandlers");
-const { getRelativePath } = require("../../../tools/function");
+const dbQuery = require('./dbUtils')
+const upload = require('./multerConfig')
+const db = require('./database')
+const { handleDbError } = require('./errorHandlers')
+const { getRelativePath } = require('../../../tools/function')
 const queryAsync = (connection, sql, params) =>
   new Promise((resolve, reject) => {
     connection.query(sql, params, (error, results) => {
       if (error) {
-        reject(error);
+        reject(error)
       } else {
-        resolve(results);
+        resolve(results)
       }
-    });
-  });
+    })
+  })
 exports.getProducts = (req, res) => {
   const query = `
     SELECT sellspec.*, product.prod_name, pi.img_src
@@ -27,13 +27,13 @@ exports.getProducts = (req, res) => {
     LEFT JOIN product_images_test pi ON pi.prod_id = first_images.prod_id AND pi.id = first_images.first_image_id
     WHERE sellspec.publish = 1 AND pi.products_info = 0
     ORDER BY sellspec.prod_id
-    `;
+    `
 
   db.query(query, (err, results) => {
-    if (err) throw err;
-    res.json(results);
-  });
-};
+    if (err) throw err
+    res.json(results)
+  })
+}
 
 exports.getDownProducts = (req, res) => {
   const query = `
@@ -43,55 +43,55 @@ exports.getDownProducts = (req, res) => {
     LEFT JOIN product_images_test ON sellspec.prod_id = product_images_test.prod_id
     WHERE sellspec.publish = 0 AND products_info = 0
     ORDER BY sellspec.prod_id
-    `;
+    `
 
   db.query(query, (err, results) => {
-    if (err) throw err;
-    res.json(results);
-  });
-};
+    if (err) throw err
+    res.json(results)
+  })
+}
 
 exports.getImage = (req, res) => {
   const query = `
     SELECT * FROM product_images_test
-    `;
+    `
 
   db.query(query, (err, results) => {
-    if (err) throw err;
-    res.json(results);
-  });
-};
+    if (err) throw err
+    res.json(results)
+  })
+}
 
 exports.updateProduct = (req, res) => {
-  const productId = req.params.productId;
-  const { publish } = req.body;
+  const productId = req.params.productId
+  const { publish } = req.body
 
   db.query(
-    "UPDATE sellspec SET publish = ? WHERE prod_id = ?",
+    'UPDATE sellspec SET publish = ? WHERE prod_id = ?',
     [publish, productId],
     (err, results) => {
       if (err) {
-        res.status(500).json({ error: "Database error" });
-        throw err;
+        res.status(500).json({ error: 'Database error' })
+        throw err
       }
 
       if (results.affectedRows === 0) {
-        res.status(404).json({ message: "Product not found" });
+        res.status(404).json({ message: 'Product not found' })
       } else {
-        res.json({ message: "成功更新資訊" });
+        res.json({ message: '成功更新資訊' })
       }
     }
-  );
-};
+  )
+}
 
 exports.createProduct = [
-  upload.array("productImage", 6),
+  upload.array('productImage', 6),
   (req, res, next) => {
-    console.log(req.body);
-    next();
+    console.log(req.body)
+    next()
   },
   async (req, res) => {
-    const mainImageIdx = Number(req.body.mainImageIdx);
+    const mainImageIdx = Number(req.body.mainImageIdx)
     const {
       productData: {
         cpu,
@@ -112,7 +112,7 @@ exports.createProduct = [
       category_id,
       transport,
       payment,
-    } = req.body;
+    } = req.body
 
     const productData = {
       user_id: req.body.productData.user_id,
@@ -121,21 +121,21 @@ exports.createProduct = [
       category_id: req.body.productData.category_id,
       transport: req.body.productData.transport,
       payment: req.body.productData.payment,
-    };
+    }
     try {
-      const results = await dbQuery("INSERT INTO product SET ?", productData);
-      const prodId = results.insertId;
-      const publishValue = req.body.productData.publish;
-      const quantityValue = req.body.productData.quantity;
-      const DEFAULT_IMAGE_PATH = "/images/products/defaultImage.jpg";
+      const results = await dbQuery('INSERT INTO product SET ?', productData)
+      const prodId = results.insertId
+      const publishValue = req.body.productData.publish
+      const quantityValue = req.body.productData.quantity
+      const DEFAULT_IMAGE_PATH = '/images/products/defaultImage.jpg'
 
-      let imagesData = [];
+      let imagesData = []
 
       if (req.files && req.files.length > 0) {
         imagesData = req.files.map((file, idx) => {
-          let relativePath;
+          let relativePath
           if (file.path) {
-            relativePath = getRelativePath(file.path);
+            relativePath = getRelativePath(file.path)
           }
           return {
             prod_id: prodId,
@@ -146,30 +146,30 @@ exports.createProduct = [
             mime_type: file.mimetype,
             img_src: relativePath,
             products_info: mainImageIdx === idx ? 0 : 1,
-          };
-        });
+          }
+        })
       } else {
         // 如果沒有上傳圖片，使用預設圖片
         imagesData.push({
           prod_id: prodId,
-          originalname: "defaultImage.jpg",
-          stored_name: "defaultImage.jpg",
+          originalname: 'defaultImage.jpg',
+          stored_name: 'defaultImage.jpg',
           upload_date: new Date(),
-          file_size: "999", // defaultData
-          mime_type: "image/jpeg",
+          file_size: '999', // defaultData
+          mime_type: 'image/jpeg',
           img_src: DEFAULT_IMAGE_PATH,
           products_info: 0, // defaultPic
-        });
+        })
       }
 
-      const imgInsertPromises = imagesData.map((imgData) => {
-        return dbQuery("INSERT INTO product_images_test SET ?", imgData);
-      });
+      const imgInsertPromises = imagesData.map(imgData => {
+        return dbQuery('INSERT INTO product_images_test SET ?', imgData)
+      })
 
-      await Promise.all(imgInsertPromises);
+      await Promise.all(imgInsertPromises)
       const sellSpecData = {
         prod_id: prodId,
-        spec_id: "10",
+        spec_id: '10',
         spec_name: spec,
         contnet: description,
         price: price,
@@ -184,157 +184,140 @@ exports.createProduct = [
         weight,
         publish: publishValue,
         stock: quantityValue,
-      };
+      }
 
-      await dbQuery("INSERT INTO sellspec SET ?", sellSpecData);
+      await dbQuery('INSERT INTO sellspec SET ?', sellSpecData)
       res.json({
         success: true,
-        message: "Product and images added successfully!",
-      });
+        message: 'Product and images added successfully!',
+      })
     } catch (error) {
-      console.error("Error:", error);
-      handleDbError(error, res);
+      console.error('Error:', error)
+      handleDbError(error, res)
     }
   },
-];
+]
 
 exports.getProductsAllData = async (req, res) => {
-  const prodId = req.params.prod_id;
+  const prodId = req.params.prod_id
 
   try {
-    const productResults = await dbQuery(
-      "SELECT * FROM product WHERE prod_id = ?",
-      [prodId]
-    );
+    const productResults = await dbQuery('SELECT * FROM product WHERE prod_id = ?', [prodId])
 
     if (productResults.length > 0) {
-      const brandId = productResults[0].brand_id;
-      const categoryId = productResults[0].category_id;
+      const brandId = productResults[0].brand_id
+      const categoryId = productResults[0].category_id
 
-      const brandResults = await dbQuery(
-        "SELECT * FROM brand WHERE brand_id = ?",
-        [brandId]
-      );
-      productResults[0].brandName =
-        brandResults.length > 0 ? brandResults[0].brand : null;
+      const brandResults = await dbQuery('SELECT * FROM brand WHERE brand_id = ?', [brandId])
+      productResults[0].brandName = brandResults.length > 0 ? brandResults[0].brand : null
 
-      const categoryResults = await dbQuery(
-        "SELECT * FROM category WHERE category_id = ?",
-        [categoryId]
-      );
+      const categoryResults = await dbQuery('SELECT * FROM category WHERE category_id = ?', [
+        categoryId,
+      ])
       productResults[0].categoryName =
-        categoryResults.length > 0 ? categoryResults[0].category : null;
+        categoryResults.length > 0 ? categoryResults[0].category : null
     }
 
-    const imagesResults = await dbQuery(
-      "SELECT * FROM product_images_test WHERE prod_id = ?",
-      [prodId]
-    );
-    const sellspecResults = await dbQuery(
-      "SELECT * FROM sellspec WHERE prod_id = ?",
-      [prodId]
-    );
+    const imagesResults = await dbQuery('SELECT * FROM product_images_test WHERE prod_id = ?', [
+      prodId,
+    ])
+    const sellspecResults = await dbQuery('SELECT * FROM sellspec WHERE prod_id = ?', [prodId])
 
     // 整合資訊並返回
     const responseData = {
       product: productResults[0],
       images: imagesResults,
       sellspec: sellspecResults,
-    };
+    }
 
-    res.json(responseData);
+    res.json(responseData)
   } catch (error) {
-    console.error("Database error:", error);
-    res.status(500).json({ error: "Failed to retrieve product details." });
+    console.error('Database error:', error)
+    res.status(500).json({ error: 'Failed to retrieve product details.' })
   }
-};
+}
 exports.partiallyUpdateProduct = (req, res) => {
-  const { prod_id } = req.params;
-  const updatedData = req.body;
-  const sellspecData = updatedData.sellspec[0];
-  const sellspecFields = [];
-  const sellspecParams = [];
+  const { prod_id } = req.params
+  const updatedData = req.body
+  const sellspecData = updatedData.sellspec[0]
+  const sellspecFields = []
+  const sellspecParams = []
 
   const fieldMapping = {
-    prod_name: "prod_name",
-    brandId: "brand_id",
-    categoryId: "category_id",
-    transport: "transport",
-    payment: "payment",
-  };
-
-  const sellspecMapping = {
-    spec_name: "spec_name",
-    price: "price",
-    cpu: "cpu",
-    gpu: "gpu",
-    ram: "ram",
-    os: "os",
-    screen: "screen",
-    battery: "battery",
-    warranty: "warranty",
-    size: "size",
-    weight: "weight",
-    stock: "stock",
-  };
-
-  if (!updatedData.prod_name) {
-    return res.status(400).json({ message: "Product name is required" });
+    prod_name: 'prod_name',
+    brandId: 'brand_id',
+    categoryId: 'category_id',
+    transport: 'transport',
+    payment: 'payment',
   }
 
-  const fields = [];
-  const params = [];
+  const sellspecMapping = {
+    spec_name: 'spec_name',
+    price: 'price',
+    cpu: 'cpu',
+    gpu: 'gpu',
+    ram: 'ram',
+    os: 'os',
+    screen: 'screen',
+    battery: 'battery',
+    warranty: 'warranty',
+    size: 'size',
+    weight: 'weight',
+    stock: 'stock',
+  }
+
+  if (!updatedData.prod_name) {
+    return res.status(400).json({ message: 'Product name is required' })
+  }
+
+  const fields = []
+  const params = []
 
   for (let key in updatedData) {
     if (fieldMapping[key]) {
-      fields.push(`${fieldMapping[key]} = ?`);
-      params.push(updatedData[key]);
+      fields.push(`${fieldMapping[key]} = ?`)
+      params.push(updatedData[key])
     }
   }
 
   for (let key in sellspecData) {
     if (sellspecMapping[key]) {
-      let value =
-        key === "price" ? Number(sellspecData[key]) : sellspecData[key];
-      sellspecFields.push(`${sellspecMapping[key]} = ?`);
-      sellspecParams.push(value);
+      let value = key === 'price' ? Number(sellspecData[key]) : sellspecData[key]
+      sellspecFields.push(`${sellspecMapping[key]} = ?`)
+      sellspecParams.push(value)
     }
   }
 
-  const sql = `UPDATE product SET ${fields.join(", ")} WHERE prod_id = ?`;
-  params.push(prod_id);
+  const sql = `UPDATE product SET ${fields.join(', ')} WHERE prod_id = ?`
+  params.push(prod_id)
 
-  const sellspecSql = `UPDATE sellspec SET ${sellspecFields.join(
-    ", "
-  )} WHERE prod_id = ?`;
-  sellspecParams.push(prod_id);
+  const sellspecSql = `UPDATE sellspec SET ${sellspecFields.join(', ')} WHERE prod_id = ?`
+  sellspecParams.push(prod_id)
 
   db.getConnection(async (err, connection) => {
     if (err) {
-      console.error("連接到資料庫時出錯: ", err.message || err);
-      return res
-        .status(500)
-        .json({ message: "內部伺服器錯誤", error: err.message });
+      console.error('連接到資料庫時出錯: ', err.message || err)
+      return res.status(500).json({ message: '內部伺服器錯誤', error: err.message })
     }
     try {
       // 使用 promisify 後的查詢函數
-      await queryAsync(connection, sql, params);
-      await queryAsync(connection, sellspecSql, sellspecParams);
+      await queryAsync(connection, sql, params)
+      await queryAsync(connection, sellspecSql, sellspecParams)
 
-      res.json({ message: "產品和 sellspect 已成功更新" });
+      res.json({ message: '產品和 sellspect 已成功更新' })
     } catch (error) {
-      console.error("資料庫操作期間出錯: ", error.message || error);
-      res.status(500).json({ message: "內部伺服器錯誤", error: error.message });
+      console.error('資料庫操作期間出錯: ', error.message || error)
+      res.status(500).json({ message: '內部伺服器錯誤', error: error.message })
     } finally {
-      connection.release();
+      connection.release()
     }
-  });
-  console.log("接收到的數據:", updatedData);
-  console.log("Executing SQL:", sql);
-  console.log("With params:", params);
+  })
+  console.log('接收到的數據:', updatedData)
+  console.log('Executing SQL:', sql)
+  console.log('With params:', params)
 
-  console.log("Executing Sellspec SQL:", sellspecSql);
-  console.log("With Sellspec params:", sellspecParams);
+  console.log('Executing Sellspec SQL:', sellspecSql)
+  console.log('With Sellspec params:', sellspecParams)
 
   // SELL更新
-};
+}
