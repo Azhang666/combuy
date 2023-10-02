b1c399b1fb5152d956f2186ef7dcc223272b1b5e
kkylerll <sdsfg0728@gmail.com>
2023-10-03 01:02:53 +0800

    新增 sweetAlert

--- server/public/js/commodityPage/productJS.js
+++ server/public/js/commodityPage/productJS.js
@@ -121,14 +121,12 @@ $(document).ready(function () {
           spec_id: spec_id,
         },
         success: function (response) {
-          console.log('已成功加入購物車');
-          alert('已成功加入購物車');
+          swal('已成功加入購物車',"請至購物車查看", "success");
           $('#cart svg').html('<svg xmlns="http://www.w3.org/2000/svg" width="20" height="" fill="currentColor" class="bi bi-cart-plus-fill" viewBox="0 0 16 16"><path d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM6 14a1 1 0 1 1-2 0 1 1 0 0 1 2 0zm7 0a1 1 0 1 1-2 0 1 1 0 0 1 2 0zM9 5.5V7h1.5a.5.5 0 0 1 0 1H9v1.5a.5.5 0 0 1-1 0V8H6.5a.5.5 0 0 1 0-1H8V5.5a.5.5 0 0 1 1 0z"/></svg>');
         },
         error: function (error) {
           console.error('加入購物車失敗', error)
-          alert('該商品已在購物車中')
-
+          swal('加入購物車失敗',"該商品以在購物車中", "error");
         },
       });
     }
@@ -188,9 +186,12 @@ $(document).ready(function () {
         },
         success: function (response) {
           console.log("已成功加入購物車");
+          swal('已成功加入購物車',"請至購物車查看", "success");
         },
         error: function (error) {
           console.error("加入購物車失敗", error);
+          swal('加入購物車失敗',"該商品以在購物車中", "error");
+
         },
       });
 
